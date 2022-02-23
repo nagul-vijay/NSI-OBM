798e62f643ac4cc68cd50dae3fc1af626621d0b2
nagul-vijay <97439038+nagul-vijay@users.noreply.github.com>
2022-02-23 19:11:28 +0530

    Add project files.

---
+++ NSI-OBM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NSI_OBM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
